7fb5d97ca67decadf11932da11380b6663870018
GeorgErich <georg.erich.1993@gmail.com>
2017-12-03 19:50:52 +0100

    1. Login erstellt. 2. ClientsideSettings erstellt 3. Weitere arbeiten: Klassen: - ClientsideSetting - LoginServiceImpl - CommonSettings - LoginInfo - LoginService - LoginServiceAsync Erstellt und vervollständigt

---
+++ src/de/hdm/gwt/carissimo/shared/LoginService.java
@@ -0,0 +1,11 @@
+package de.hdm.gwt.carissimo.shared;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("login")
+	public interface LoginService extends RemoteService {
+	
+		public LoginInfo login(String requestUri);
+}
+
