513c15856d411dfb05880f09782f78945a231ac0
Batista <kc015@hdm-stuttgart.de>
2017-12-17 18:39:29 +0100

    Kleine Anpassungen (JavaDoc)

--- src/de/hdm/gwt/carissimo/client/gui/ProfilBearbeiten.java
+++ src/de/hdm/gwt/carissimo/client/gui/ProfilBearbeiten.java
@@ -4,20 +4,15 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 /**
- * 
- * Ansicht für Profilbearbeitung
+ * Klasse die zur Ansicht für die Profilbearbeitung dient
  * @author Kevin C. Batista
  */
 public class ProfilBearbeiten extends Composite {
 	
 	VerticalPanel vPanel = new VerticalPanel();
 
-
-
 	public ProfilBearbeiten(){
 		initWidget(vPanel);
-
-
 
 		Label label = new Label("Du befindest dich hier bei: Profil bearbeiten");
 		vPanel.add(label);
