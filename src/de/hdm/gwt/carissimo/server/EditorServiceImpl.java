36178b62dee2bd9a766b1442d06d7dd15bbb9332
YakupKanal <yk014@hdm-stuttgart.de>
2017-12-17 16:05:49 +0100

    Implementationsklasse angefangen + kleine Anpassungen

--- src/de/hdm/gwt/carissimo/server/EditorServiceImpl.java
+++ src/de/hdm/gwt/carissimo/server/EditorServiceImpl.java
@@ -1,9 +1,180 @@
 package de.hdm.gwt.carissimo.server;
 
+import java.util.Date;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.gwt.carissimo.server.db.AuswahleigenschaftMapper;
+import de.hdm.gwt.carissimo.server.db.BesuchMapper;
+import de.hdm.gwt.carissimo.server.db.EigenschaftMapper;
+import de.hdm.gwt.carissimo.server.db.FreitexteigenschaftMapper;
+import de.hdm.gwt.carissimo.server.db.InfoMapper;
+import de.hdm.gwt.carissimo.server.db.KontaktsperreMapper;
+import de.hdm.gwt.carissimo.server.db.MerkzettelMapper;
+import de.hdm.gwt.carissimo.server.db.ProfilMapper;
+import de.hdm.gwt.carissimo.server.db.SuchprofilMapper;
 import de.hdm.gwt.carissimo.shared.EditorService;
+import de.hdm.gwt.carissimo.shared.bo.Profil;
 
 public class EditorServiceImpl extends RemoteServiceServlet implements EditorService{
 
+	// Instanzvariablen: Mapperklassen
+	private ProfilMapper pMapper = ProfilMapper.profilMapper();
+	private InfoMapper iMapper = InfoMapper.infoMapper();
+	private EigenschaftMapper eMapper = EigenschaftMapper.eigenschaftMapper();
+	private AuswahleigenschaftMapper aMapper = AuswahleigenschaftMapper.auswahleigenschaftMapper();
+	private FreitexteigenschaftMapper fMapper = FreitexteigenschaftMapper.freitexteigenschaftMapper();
+	private SuchprofilMapper spMapper = SuchprofilMapper.suchprofilMapper();
+//	private SuchprofilInfoMapper spiMapper = SuchprofilInfoMapper.suchprofilInfoMapper();
+	private BesuchMapper bMapper = BesuchMapper.besuchMapper();
+	private MerkzettelMapper mMapper = MerkzettelMapper.merkzettelMapper();
+	private KontaktsperreMapper kMapper = KontaktsperreMapper.kontaktsperreMapper();
+
+	
+	// Instanzvariable: Eingeloggter User
+	private Profil user;
+	
+	
+	
+	/*****************************************************************************************************
+	 * START: Profil
+	 ****************************************************************************************************/
+	
+	/**
+	 * Profil
+	 */
+	public void setUser(Profil p) {
+		user = p;
+	}
+
+	/**
+	 * Profil anlegen
+	 * 
+	 * @param Profilattribute
+	 */
+	public void insertProfil(String email, String vorname, String nachname, String geschlecht, Date geburtsdatum,
+			int koerpergroesse, String haarfarbe, String raucher, String religion, String beschreibung) throws Exception {
+		
+		Profil p = new Profil();
+		
+		p.setEmail(email);
+		p.setVorname(vorname);
+		p.setNachname(nachname);
+		p.setGeschlecht(geschlecht);
+		p.setGeburtsdatum(geburtsdatum);
+		p.setKoerpergroesse(koerpergroesse);
+		p.setHaarfarbe(haarfarbe);
+		p.setRaucher(raucher);
+		p.setReligion(religion);
+		pMapper.insertProfil(p);
+	}
+	
+	
+	// Methode (2)
+
+	// Methode (3)
+	
+	// ...
+	
+	
+	
+	/***************************************************************************************************** 
+	 * ENDE: Profil
+	 ****************************************************************************************************/
+	
+	
+	/*****************************************************************************************************
+	 * START: Eigenschaft, Auswahl & Info
+	 ****************************************************************************************************/
+	
+	// Methode (1)
+	
+	// Methode (2)
+
+	// Methode (3)
+	
+	
+	/***************************************************************************************************** 
+	 * ENDE: Eigenschaft, Auswahl & Info
+	 ****************************************************************************************************/
+	
+	
+	/*****************************************************************************************************
+	 * START: Suchprofil & SuchprofilInfo
+	 ****************************************************************************************************/
+	
+	// Methode (1)
+	
+	// Methode (2)
+
+	// Methode (3)
+	
+	
+	/***************************************************************************************************** 
+	 * ENDE: Suchprofil & SuchprofilInfo
+	 ****************************************************************************************************/
+	
+	
+	/*****************************************************************************************************
+	 * START: Besuch
+	 ****************************************************************************************************/
+	
+	// Methode (1)
+	
+	// Methode (2)
+
+	// Methode (3)
+	
+	
+	/***************************************************************************************************** 
+	 * ENDE: Besuch
+	 ****************************************************************************************************/
+
+
+	/*****************************************************************************************************
+	 * START: Merkzettel
+	 ****************************************************************************************************/
+	
+	// Methode (1)
+	
+	// Methode (2)
+
+	// Methode (3)
+	
+	
+	/***************************************************************************************************** 
+	 * ENDE: Merkzettel
+	 ****************************************************************************************************/
+
+	
+	/*****************************************************************************************************
+	 * START: Kontaktsperre
+	 ****************************************************************************************************/
+	
+	// Methode (1)
+	
+	// Methode (2)
+
+	// Methode (3)
+	
+	
+	/***************************************************************************************************** 
+	 * ENDE: Kontaktsperre
+	 ****************************************************************************************************/
+	
+
+	/*****************************************************************************************************
+	 * START: Sonstiges
+	 ****************************************************************************************************/
+	
+	// Methode (1)
+	
+	// Methode (2)
+
+	// Methode (3)
+	
+	
+	/***************************************************************************************************** 
+	 * ENDE: Sonstiges
+	 ****************************************************************************************************/	
 }
--- src/de/hdm/gwt/carissimo/server/db/ProfilMapper.java
+++ src/de/hdm/gwt/carissimo/server/db/ProfilMapper.java
@@ -56,7 +56,7 @@ public class ProfilMapper {
 	 * Einf�gen eines Kontaktsperren-Objekts in die Datenbank.
 	 */
 	
-	public void insertProfilMapper(Profil p) throws Exception{
+	public void insertProfil(Profil p) throws Exception{
 		
 		Connection con = (Connection) DBConnection.connection();
 		
@@ -69,7 +69,7 @@ public class ProfilMapper {
 				+ p.getNachname() + "','"
 				+ p.getGeschlecht() + "','"
 				+ p.getGeburtsdatum()+ "','"
-				+ p.getKoepergroesse() + "','"
+				+ p.getKoerpergroesse() + "','"
 				+ p.getHaarfarbe() + "','"
 				+ p.getRaucher() + "','"
 				+ p.getReligion() + "','"				
@@ -93,7 +93,7 @@ public class ProfilMapper {
 				+ "nachname = '" + p.getNachname() + "', "
 				+ "geschlecht = '" + p.getGeschlecht() + "', "
 			    + "geburtsdatum = '" + p.getGeburtsdatum() + "', "
-				+ "koerpergroesse = '" + p.getKoepergroesse() + "', "
+				+ "koerpergroesse = '" + p.getKoerpergroesse() + "', "
 				+ "haarfarbe = '" + p.getHaarfarbe() + "', "
 				+ "raucher = '" + p.getRaucher() + "', "
 				+ "religion = '" + p.getReligion() + "', "
@@ -143,7 +143,7 @@ public class ProfilMapper {
 			profil.setGeburtsdatum(result.getDate("geburtsdatum"));
 			profil.setGeschlecht(result.getString("geschlecht"));
 			profil.setHaarfarbe(result.getString("haarfarbe"));
-			profil.setKoepergroesse(result.getInt("koerpergroesse"));
+			profil.setKoerpergroesse(result.getInt("koerpergroesse"));
 			profil.setReligion(result.getString("religion"));
 			profil.setRaucher(result.getString("raucher"));
 				
@@ -172,7 +172,7 @@ public class ProfilMapper {
 			profil.setGeburtsdatum(result.getDate("geburtsdatum"));
 			profil.setGeschlecht(result.getString("geschlecht"));
 			profil.setHaarfarbe(result.getString("haarfarbe"));
-			profil.setKoepergroesse(result.getInt("koerpergroesse"));
+			profil.setKoerpergroesse(result.getInt("koerpergroesse"));
 			profil.setReligion(result.getString("religion"));
 			profil.setRaucher(result.getString("raucher"));
 		}
