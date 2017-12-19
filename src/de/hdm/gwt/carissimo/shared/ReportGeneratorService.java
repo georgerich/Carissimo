1ed965d04982a08c679806e1be2a0836c4d92a7f
GeorgErich <georg.erich.1993@gmail.com>
2017-12-19 21:54:49 +0100

    Importfehler behoben

--- src/de/hdm/gwt/carissimo/shared/ReportGeneratorService.java
+++ src/de/hdm/gwt/carissimo/shared/ReportGeneratorService.java
@@ -1,6 +1,7 @@
 package de.hdm.gwt.carissimo.shared;
 
 import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("report")
 public interface ReportGeneratorService extends RemoteService {
